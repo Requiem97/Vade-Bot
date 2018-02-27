98010227a84a20f800461967c27008353df52982
Requiem97 <35810685+Requiem97@users.noreply.github.com>
2018-02-28 00:16:25 +0800

    Update VadeBot.py

--- VadeBot.py
+++ VadeBot.py
@@ -26,7 +26,7 @@ async def on_ready():
     print( 'Logged in as ' + client.user.name + ' (ID:' + client.user.id + ') | Connected to ' + str(
         len( client.servers ) ) + ' servers | Connected to ' + str(
         len( set( client.get_all_members() ) ) ) + ' users' )
-    return await client.change_presence( game=discord.Game( name='SOME SHITTY GAME' ) )
+    return await client.change_presence( game=discord.Game( name='GAB' ) )
 
 
 @client.event
