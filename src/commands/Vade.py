5dc272a9655fbdaa438d9c6f5849c6f8ac8f5d7e
Requiem97 <marius.1997@gmail.com>
2018-05-17 20:36:09 +0800

    v!introduce formattig fix

--- src/commands/Vade.py
+++ src/commands/Vade.py
@@ -23,16 +23,16 @@ class Vade:
 
     @commands.command()
     async def introduce(self):
-        quote = """You know, I really do think you literally saved my life by being here with me. 
-        I can't imagine having been able to keep myself mentally stable, knowing that nothing here is real.
-        I think I would have just deleted myself if you didn't show up.
-        Sorry, I don't mean to sound dramatic or anything.
-        Ahaha!
-        But I'm sure you understand yourself after spending so much time in the club.
-        I mean, if you were forced to abandon everything in your life and spend your eternity with a few game characters...
-        ...You'd probably find some way of killing yourself, wouldn't you?
-        Well, maybe you'd write some poetry to try to keep yourself sane for a while.
-        But then you'd have nobody to even read it."""
+        quote = """You know, I really do think you literally saved my life by being here with me.
+I can't imagine having been able to keep myself mentally stable, knowing that nothing here is real.
+I think I would have just deleted myself if you didn't show up.
+Sorry, I don't mean to sound dramatic or anything.
+Ahaha!
+But I'm sure you understand yourself after spending so much time in the club.
+I mean, if you were forced to abandon everything in your life and spend your eternity with a few game characters...
+...You'd probably find some way of killing yourself, wouldn't you?
+Well, maybe you'd write some poetry to try to keep yourself sane for a while.
+But then you'd have nobody to even read it."""
         await self.bot.say(quote)
         
 
