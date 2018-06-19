422aa0e516782b361410fe86009f5f5b73e9e391
Requiem97 <marius.1997@gmail.com>
2018-06-19 14:56:06 +0800

    fixing errors

--- src/commands/Card.py
+++ src/commands/Card.py
@@ -7,22 +7,27 @@ class Card:
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.command()
-    async def card (self, cmd, card):
+    @commands.group(pass_context=True)
+    async def card (self, ctx):
         "v!card view [card] to view specified card\nv!card list to view all cards"
-        if cmd != "view" and cmd != "list" :
+        if ctx.invoked_subcommand is None:
             await self.bot.say("BAWAL YAN BOBO!")
-        elif cmd == "view":
-            if card == None:
-                pass
-            else:
-                await self.bot.upload(VadeDeets.cardList[VadeDeets.cardMap.index(card)])
-        elif cmd == "list":
-            commonCards = [x.endswith("1") for x in VadeDeets.cardMap]
-            uncommonCards = [x.endswith("2") for x in VadeDeets.cardMap]
-            rareCards = [x.endswith("3") for x in VadeDeets.cardMap]
-            specialCards = [x.endswith("4") for x in VadeDeets.cardMap]
-            await self.bot.say("__**Common Cards**__\n", (x for x in commonCards))
+
+            
+    @card.command()
+    async def view(self, card):
+        if card == None:
+            pass
+        else:
+            await self.bot.upload(VadeDeets.cardList[VadeDeets.cardMap.index(card)])
+    @card.command()
+    async def list(self):
+        commonCards = [x.endswith("1") for x in VadeDeets.cardMap]
+        uncommonCards = [x.endswith("2") for x in VadeDeets.cardMap]
+        rareCards = [x.endswith("3") for x in VadeDeets.cardMap]
+        specialCards = [x.endswith("4") for x in VadeDeets.cardMap]
+        await self.bot.say("__**Common Cards**__\n", (x for x in commonCards))
+
 
 def setup(bot):
     bot.add_cog(Card(bot))
