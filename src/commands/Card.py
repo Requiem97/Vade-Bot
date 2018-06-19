304fb988cecaed74c0c97476ecb08a88fb77043d
Requiem97 <marius.1997@gmail.com>
2018-06-19 14:46:36 +0800

    testing card list function

--- src/commands/Card.py
+++ src/commands/Card.py
@@ -9,12 +9,20 @@ class Card:
     
     @commands.command()
     async def card (self, cmd, card):
-        if cmd != "view":
+        "v!card view [card] to view specified card\nv!card list to view all cards"
+        if cmd != "view" and cmd != "list" :
             await self.bot.say("BAWAL YAN BOBO!")
-        elif card == None:
-            pass
-        else:
-            await self.bot.upload(VadeDeets.cardList[VadeDeets.cardMap.index(card)])
+        elif cmd == "view":
+            if card == None:
+                pass
+            else:
+                await self.bot.upload(VadeDeets.cardList[VadeDeets.cardMap.index(card)])
+        elif cmd == "list":
+            commonCards = [x.endswith("1") for x in VadeDeets.cardMap]
+            uncommonCards = [x.endswith("2") for x in VadeDeets.cardMap]
+            rareCards = [x.endswith("3") for x in VadeDeets.cardMap]
+            specialCards = [x.endswith("4") for x in VadeDeets.cardMap]
+            await self.bot.say("__**Common Cards**__\n", (x for x in commonCards))
 
 def setup(bot):
     bot.add_cog(Card(bot))
