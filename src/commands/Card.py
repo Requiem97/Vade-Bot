528b7196ad9ef9ffb894bd3fb05e6a2b429c9553
Requiem97 <marius.1997@gmail.com>
2018-06-19 15:16:55 +0800

    fixed v!card list

--- src/commands/Card.py
+++ src/commands/Card.py
@@ -22,12 +22,15 @@ class Card:
             await self.bot.upload(VadeDeets.cardList[VadeDeets.cardMap.index(card)])
     @card.command()
     async def list(self):
-        commonCards = [x.endswith("1") for x in VadeDeets.cardMap]
-        uncommonCards = [x.endswith("2") for x in VadeDeets.cardMap]
-        rareCards = [x.endswith("3") for x in VadeDeets.cardMap]
-        specialCards = [x.endswith("4") for x in VadeDeets.cardMap]
-        commonText = "__**Common Cards**__\n" + commonCards.strip("['']")
-        await self.bot.say(commonText)
+        commonCards = [card for card in VadeDeets.cardMap if card.endswith("1")]
+        uncommonCards = [card for card in VadeDeets.cardMap if card.endswith("2")]
+        rareCards = [card for card in VadeDeets.cardMap if card.endswith("3")]
+        specialCards = [card for card in VadeDeets.cardMap if card.endswith("4")]
+        commonText = "__**Common Cards**__\n" + str(commonCards).strip("[']")
+        uncommonText = "__**Uncommon Cards**__\n" + str(uncommonCards).strip("[']")
+        rareText = "__**Rare Cards**__\n" + str(rareCards).strip("[']")
+        specialText = "__**Special Cards**__\n" + str(specialCards).strip("[']")
+        await self.bot.say(commonText + "\n" + uncommonText + "\n" + rareText + "\n" + specialText)
 
 
 def setup(bot):
