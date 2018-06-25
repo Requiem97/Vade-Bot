12a82398aa6fa4fa41248d12351e5899fac83ff8
Requiem97 <marius.1997@gmail.com>
2018-06-26 01:51:16 +0800

    reverted commit due to bug

--- src/commands/Card.py
+++ src/commands/Card.py
@@ -26,9 +26,8 @@ class Card:
     @commands.command()
     async def fund(self):
         "Get personal fund balance"
-        #connect.getFund(VadeDeets.userID)
-        fund = connect.getFund(VadeDeets.userID)
-        print(fund)
+        connect.getFund(VadeDeets.userID)
+        #fund = connect.getFund(VadeDeets.userID)
         await self.bot.say ("You currently got " + VadeDeets.fund + " Php from the fund")
     
     @commands.group(pass_context=True)
--- src/db/connect.py
+++ src/db/connect.py
@@ -2,14 +2,13 @@ import os, psycopg2, datetime
 from src.commands import VadeDeets 
 
 conn = None
-cur = None
+
 
 def connect():
     global conn
     db_url = os.environ['DATABASE_URL']
     try:
         conn = psycopg2.connect(db_url, sslmode='require')
-        cur = conn.cursor()
         print("success")
     except:
         print("unable to connect to db")
@@ -17,7 +16,8 @@ def connect():
 
 def uploadData(userID, amount, dt):
     print("updating data")
-    global conn, cur
+    global conn
+    cur = conn.cursor()
     cur.execute("SELECT fund from bot.daily WHERE user_id = %s;", (str(userID),))
     row = cur.fetchall()
     fund = int(row[0][0])
@@ -29,21 +29,24 @@ def uploadData(userID, amount, dt):
 
 def hasData(userId):
     print("checking data")
-    global conn, cur
+    global conn
+    cur = conn.cursor()
     cur.execute("SELECT * from bot.daily WHERE user_id = %s;", (str(userId),))
     print("success")
     return True if cur.fetchone() else False
 
 def createData(userID, amount, dt):
     print("creating data")
-    global conn, cur
+    global conn
+    cur=conn.cursor()
     cur.execute("INSERT into bot.daily (user_id, fund, last_used) VALUES(%s, %s, %s)", (str(userID), amount, dt,))
     conn.commit()
     print("success")
 
 def canUse(userID):
     print("checking if v!daily is allowed")
-    global conn, cur
+    global conn
+    cur=conn.cursor()
     cur.execute("SELECT last_used from bot.daily WHERE user_id = %s;", (str(userID),))
     row = cur.fetchall()
     last_used = row[0][0]
@@ -58,7 +61,8 @@ def canUse(userID):
 
 def getFund(userID):
     print("checking fund data")
-    global conn, cur
+    global conn
+    cur = conn.cursor()
     if hasData(userID):
         print("has data")
         cur.execute("SELECT fund from bot.daily WHERE user_id = %s;", (str(userID),))
@@ -66,8 +70,6 @@ def getFund(userID):
         fund = row[0][0]
         print(fund)
         VadeDeets.fund = str(fund)
-        return str(fund)
     else:
         print("no data")
         VadeDeets.fund = str(0)
-        return str(0)
