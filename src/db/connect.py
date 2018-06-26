b187a5230c63762d1a349a0a817edd63094e83af
Requiem97 <marius.1997@gmail.com>
2018-06-26 19:50:20 +0800

    reformatted code

--- src/db/connect.py
+++ src/db/connect.py
@@ -2,13 +2,15 @@ import os, psycopg2, datetime
 from src.commands import VadeDeets 
 
 conn = None
+cur = None
 
 
 def connect():
-    global conn
+    global conn, cur
     db_url = os.environ['DATABASE_URL']
     try:
         conn = psycopg2.connect(db_url, sslmode='require')
+        cur = conn.cursor()
         print("success")
     except:
         print("unable to connect to db")
@@ -16,8 +18,7 @@ def connect():
 
 def uploadData(userID, amount, dt):
     print("updating data")
-    global conn
-    cur = conn.cursor()
+    global conn, cur
     cur.execute("SELECT fund from bot.daily WHERE user_id = %s;", (str(userID),))
     row = cur.fetchall()
     fund = int(row[0][0])
@@ -29,30 +30,28 @@ def uploadData(userID, amount, dt):
 
 def hasData(userId):
     print("checking data")
-    global conn
-    cur = conn.cursor()
+    global conn, cur
     cur.execute("SELECT * from bot.daily WHERE user_id = %s;", (str(userId),))
     print("success")
     return True if cur.fetchone() else False
 
 def createData(userID, amount, dt):
     print("creating data")
-    global conn
-    cur=conn.cursor()
+    global conn, cur
     cur.execute("INSERT into bot.daily (user_id, fund, last_used) VALUES(%s, %s, %s)", (str(userID), amount, dt,))
     conn.commit()
     print("success")
 
 def canUse(userID):
     print("checking if v!daily is allowed")
-    global conn
-    cur=conn.cursor()
+    global conn, cur
     cur.execute("SELECT last_used from bot.daily WHERE user_id = %s;", (str(userID),))
     row = cur.fetchall()
     last_used = row[0][0]
     current = datetime.datetime.now()
     delta = current - last_used
-    VadeDeets.wait = str(datetime.timedelta(seconds = 86400 - delta.seconds))
+    delta = datetime.timedelta(seconds = 75600 - delta.seconds)
+    VadeDeets.wait = str(datetime.timedelta(seconds = 75600 - delta.seconds))
     print("success", delta)
     if delta.days >= 1:
         return True 
@@ -61,8 +60,7 @@ def canUse(userID):
 
 def getFund(userID):
     print("checking fund data")
-    global conn
-    cur = conn.cursor()
+    global conn, cur
     if hasData(userID):
         print("has data")
         cur.execute("SELECT fund from bot.daily WHERE user_id = %s;", (str(userID),))
