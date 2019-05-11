762db759d491fdbc88514f8c539d57e2a89ec14f
Requiem97 <marius.1997@gmail.com>
2019-05-11 15:11:24 +0800

    fixed rank display issue

--- src/commands/Rainbow6.py
+++ src/commands/Rainbow6.py
@@ -9,26 +9,25 @@ class Rainbow6:
     @commands.command()
     async def r6(self, user):
         url = 'https://r6.tracker.network/profile/pc/{}'.format(user.lower())
-        print(url)
         request = requests.get(url)
         scrape = BeautifulSoup(request.text, 'html.parser')
         if request.status_code == 200: 
             rank = scrape.select("#profile > div.trn-scont.trn-scont--swap > div.trn-scont__content > div:nth-child(4) > div.r6-season-list > div > div.r6-season__info > div:nth-child(3) > div.trn-text--dimmed.trn-text--center")
             profile_pic = scrape.select("#profile > div.trn-profile-header.trn-card > div > div.trn-profile-header__avatar.trn-roundavatar.trn-roundavatar--white > img")[0]['src']
-            wins = scrape.find("div", { "data-stat" : "PVPMatchesWon" }).text
-            win_ratio = scrape.find("div", { "data-stat" : "PVPWLRatio" }).text
-            pvp_kills = scrape.find("div", { "data-stat" : "PVPKills" }).text
-            pvp_kd = scrape.find("div", { "data-stat" : "PVPKDRatio" }).text
-            loses = scrape.find("div", { "data-stat" : "PVPMatchesLost" }).text
-            embed = discord.Embed(title="{!s} Rainbow 6 Siege stats".format(user), colour=discord.Colour(0x4e07b3), url="https://discordapp.com", description="These are the general stats of {!s}  in Rainbow Six Siege".format(user))
+            wins = scrape.find("div", { "data-stat" : "PVPMatchesWon" })
+            win_ratio = scrape.find("div", { "data-stat" : "PVPWLRatio" })
+            pvp_kills = scrape.find("div", { "data-stat" : "PVPKills" })
+            pvp_kd = scrape.find("div", { "data-stat" : "PVPKDRatio" })
+            loses = scrape.find("div", { "data-stat" : "PVPMatchesLost" })
+            embed = discord.Embed(title="{!s} Rainbow 6 Siege stats".format(user), colour=discord.Colour(0x4e07b3), url=url, description="These are the general stats of {!s}  in Rainbow Six Siege".format(user))
             embed.set_thumbnail(url=profile_pic)
             if rank:
-                embed.add_field(name="Rank", value=rank[0])
-            embed.add_field(name="Wins", value=wins, inline=True)
-            embed.add_field(name="Loses", value=loses, inline=True)
-            embed.add_field(name="Win Ratio", value=win_ratio)
-            embed.add_field(name="Kills", value=pvp_kills, inline=True)
-            embed.add_field(name="K/D", value=pvp_kd, inline=True)
+                embed.add_field(name="Rank", value=rank[0].text)
+            embed.add_field(name="Wins", value=wins.text, inline=True)
+            embed.add_field(name="Loses", value=loses.text, inline=True)
+            embed.add_field(name="Win Ratio", value=win_ratio.text)
+            embed.add_field(name="Kills", value=pvp_kills.text, inline=True)
+            embed.add_field(name="K/D", value=pvp_kd.text, inline=True)
             await self.bot.say(embed=embed)
         elif request.status_code == 404:
             print("user does not exist")
