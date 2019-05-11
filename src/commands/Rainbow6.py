cd7c016ac83fb63b4d3bb8219be70f851c4a9bf9
Requiem97 <marius.1997@gmail.com>
2019-05-12 02:30:27 +0800

    fixed r6 command format and added data

--- src/commands/Rainbow6.py
+++ src/commands/Rainbow6.py
@@ -2,35 +2,77 @@ import discord
 from discord.ext import commands
 import requests
 from bs4 import BeautifulSoup
+
+
 class Rainbow6:
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.command()
     async def r6(self, user):
+        "Views general Rainbow 6 Siege stats of specified user"
         url = 'https://r6.tracker.network/profile/pc/{}'.format(user.lower())
         request = requests.get(url)
         scrape = BeautifulSoup(request.text, 'html.parser')
-        if request.status_code == 200: 
-            rank = scrape.select("#profile > div.trn-scont.trn-scont--swap > div.trn-scont__content > div:nth-child(4) > div.r6-season-list > div > div.r6-season__info > div:nth-child(3) > div.trn-text--dimmed.trn-text--center")
-            profile_pic = scrape.select("#profile > div.trn-profile-header.trn-card > div > div.trn-profile-header__avatar.trn-roundavatar.trn-roundavatar--white > img")[0]['src']
-            wins = scrape.find("div", { "data-stat" : "PVPMatchesWon" })
-            win_ratio = scrape.find("div", { "data-stat" : "PVPWLRatio" })
-            pvp_kills = scrape.find("div", { "data-stat" : "PVPKills" })
-            pvp_kd = scrape.find("div", { "data-stat" : "PVPKDRatio" })
-            loses = scrape.find("div", { "data-stat" : "PVPMatchesLost" })
-            embed = discord.Embed(title="{!s} Rainbow 6 Siege stats".format(user), colour=discord.Colour(0x4e07b3), url=url, description="These are the general stats of {!s}  in Rainbow Six Siege".format(user))
-            embed.set_thumbnail(url=profile_pic)
+        if request.status_code == 200:
+            #About Data
+            user = scrape.select("div.trn-profile-header.trn-card > div > h1 > span")
+            rank = scrape.select(
+                "div.trn-scont.trn-scont--swap > div.trn-scont__content > div:nth-child(4) > div.r6-season-list > div > div.r6-season__info > div:nth-child(3) > div.trn-text--dimmed.trn-text--center")
+            profile_pic = scrape.select(
+                "div.trn-profile-header.trn-card > div > div.trn-profile-header__avatar.trn-roundavatar.trn-roundavatar--white > img")
+            level = scrape.select("div.trn-card__content.trn-card--light.trn-defstats-grid > div:nth-child(1) > div > div.trn-defstat__value")
+            pvp_time_played = scrape.find("div", { "data-stat" : "PVPTimePlayed" })
+
+            #Win/Loss data
+            pvp_wins = scrape.find("div", {"data-stat": "PVPMatchesWon"})
+            pvp_loses = scrape.find("div", {"data-stat": "PVPMatchesLost"})
+            pvp_wl_ratio = scrape.find("div", {"data-stat": "PVPWLRatio"})
+
+            #Kill/Death data
+            pvp_kills = scrape.find("div", {"data-stat": "PVPKills"})
+            pvp_deaths = scrape.find("div", {"data-stat": "PVPDeaths"})
+            pvp_kd = scrape.find("div", {"data-stat": "PVPKDRatio"})
+            pvp_headshots = scrape.find("div", {"data-stat": "PVPHeadshots"})
+            pvp_melee = scrape.find("div", {"data-stat": "PVPMeleeKills"})
+            pvp_blind = scrape.find("div", {"data-stat": "PVPBlindKills"})
+
+            embed = discord.Embed(title="General Rainbow Six Siege Stats", colour=discord.Colour(0x903c31), url=url)
+            embed.set_thumbnail(url=profile_pic[0]['src'])
+            embed.set_author(name=user[0].text.strip(),
+                             icon_url=profile_pic[0]['src'])
+            embed.set_footer(text="Stats taken from r6.tracker.network")
+
+            #About Field
             if rank:
-                embed.add_field(name="Rank", value=rank[0].text)
-            embed.add_field(name="Wins", value=wins.text, inline=True)
-            embed.add_field(name="Loses", value=loses.text, inline=True)
-            embed.add_field(name="Win Ratio", value=win_ratio.text)
-            embed.add_field(name="Kills", value=pvp_kills.text, inline=True)
-            embed.add_field(name="K/D", value=pvp_kd.text, inline=True)
+                embed.add_field(name="About", 
+                                value="**Level:** {}\n".format(level[0].text.strip()) \
+                                    +"**Time Played:** {}\n".format(pvp_time_played.text.strip()) \
+                                    +"**Rank:** {}".format(rank[0].text.strip()))
+
+            else:
+                embed.add_field(name="About", 
+                                value="**Level:** {}\n".format(level[0].text.strip())  \
+                                    +"**Time Played:** {}\n".format(pvp_time_played.text.strip()))
+
+            #Win/Loss field
+            embed.add_field(name="Win/Loss",
+                            value="**Wins:** {}\n".format(pvp_wins.text.strip()) \
+                                + "**Loses:** {}\n".format(pvp_loses.text.strip()) \
+                                + "**W/R:** {}".format(pvp_wl_ratio.text.strip()), 
+                            inline=True)
+            
+            #Kill/Death field
+            embed.add_field(name="Kills/Deaths",
+                            value="**Kills:** {}\n".format(pvp_kills.text.strip()) \
+                                + "**Deaths:** {}\n".format(pvp_deaths.text.strip()) \
+                                + "**K/D:** {}\n".format(pvp_kd.text.strip()) \
+                                + "**Headshots:** {}\n".format(pvp_headshots.text.strip()) \
+                                + "**Melee Kills:** {}\n".format(pvp_melee.text.strip()) \
+                                + "**Blind Kills:** {}".format(pvp_blind.text.strip()), 
+                            inline=True)
             await self.bot.say(embed=embed)
         elif request.status_code == 404:
-            print("user does not exist")
             await self.bot.say("User does not exist")
         else:
             await self.bot.say("Some error has occured")
