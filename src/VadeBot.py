287499d2702d12ee4f9d0bf24a612e2d792841f4
Marius Reyes <marius.1997@gmail.com>
2020-04-18 17:15:47 +0800

    [#2] Updated code to support discord.py rewrite

--- src/VadeBot.py
+++ src/VadeBot.py
@@ -61,6 +61,7 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
+    await bot.process_commands(message)
     user_id = message.author.id
     print(message)
     words = message.content.lower().split()
@@ -69,22 +70,22 @@ async def on_message(message):
     #    await bot.add_reaction(message, u"\u27A1")
     #    await asyncio.sleep(3)
     #    await bot.add_reaction(message, 	u"\U0001F496")
-    if user_id != bot.user.id and message.guild.id != 607181202922799135:
-        if message.content.startswith('v!8ball'):
-            if len(words) == 1:
-                await message.channel.send("THAT AIN'T A FUCKING QUESTION FFS")
-            else:
-                await message.channel.send(random.choice(ball_replies))
-        elif message.content.lower() == "good vade":
-            await message.channel.send(file=discord.File('src/pics/vadesmile.jpg'))
-        elif message.content.lower() == "bad vade":
-            await message.channel.send(file=discord.File('src/pics/badvade.jpg'))
-        elif find_Bobo(words):
-            await message.channel.send(bobo_tag(user_id))
-        elif random.randint(1, 100) <= 3:
-            msg = random.choice(messages)
-            await message.channel.send(msg)
-    await bot.process_commands(message)
+    # if user_id != bot.user.id and message.guild.id != 607181202922799135: #start
+    #     if message.content.startswith('v!8ball'):
+    #         if len(words) == 1:
+    #             await message.channel.send("THAT AIN'T A FUCKING QUESTION FFS")
+    #         else:
+    #             await message.channel.send(random.choice(ball_replies))
+    #     elif message.content.lower() == "good vade":
+    #         await message.channel.send(file=discord.File('src/pics/vadesmile.jpg'))
+    #     elif message.content.lower() == "bad vade":
+    #         await message.channel.send(file=discord.File('src/pics/badvade.jpg'))
+    #     elif find_Bobo(words):
+    #         await message.channel.send(bobo_tag(user_id))
+    #     elif random.randint(1, 100) <= 3:
+    #         msg = random.choice(messages)
+    #         await message.channel.send(msg)
+    # await bot.process_commands(message)
 
 if __name__ == '__main__':
     print('Checking for Command cogs')
